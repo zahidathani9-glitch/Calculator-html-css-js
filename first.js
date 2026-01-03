25b555e05e5cafde74dd68058961dbc4773b5556
Zahid Akhtar <zahidathani9@gmail.com>
2026-01-03 14:14:18 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,80 @@
+const currDisplay = document .querySelector(".curr-display");
+const prevDisplay = document .querySelector(".prev-display");
+const numbers = document .querySelectorAll(".number");
+const operands = document .querySelectorAll(".operation");
+const clearBtn = document .querySelector(".clear");
+const delBtn = document .querySelector(".delete");
+const equalBtn = document .querySelector(".equal");
+let operation ;
+
+function appendNumber(number) {
+    if (number === "." && currDisplay.innerText.includes(".")) return;
+    currDisplay.innerText +=number;
+}
+
+function chooseoperation(operand) {
+    if (currDisplay.innerText === "") return;
+    compute(operand);
+    operation = operand;
+    currDisplay.innerText += operand;
+    prevDisplay.innerText = currDisplay.innerText;
+    currDisplay.innerText = "";
+}
+
+function clearDisplay() {
+    currDisplay.innerText = "";
+    prevDisplay.innerText = "";
+}
+
+function compute(operand) {
+    let result;
+    const previousValue = parseFloat(prevDisplay.innerText);
+    const currentvalue = parseFloat(currDisplay.innerText);
+    
+    if (isNaN(previousValue) || isNaN(currentvalue)) return;
+
+    switch (operation) {
+        case "+":
+         result = previousValue + currentvalue;
+         break;
+         case "-":
+         result = previousValue - currentvalue;
+         break;
+         case "*":
+         result = previousValue * currentvalue;
+         break;
+         case "/":
+         result = previousValue / currentvalue;
+         break;
+         default: 
+         return;
+    }
+    currDisplay.innerText = result;
+}
+
+numbers.forEach((number) => {
+   number.addEventListener("click", () =>{
+    appendNumber(number.innerText)
+   });
+});
+
+operands.forEach((operand) => {
+    operand.addEventListener("click", () => {
+        chooseoperation(operand.innerText);
+    });
+});
+
+
+clearBtn.addEventListener("click", () => {
+    clearDisplay();
+});
+
+equalBtn.addEventListener("click", () => {
+    compute ();
+    prevDisplay.innerText = "";
+});
+
+delBtn.addEventListener("click", () => {
+    currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+});
+
